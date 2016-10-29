6922b6f0de7340dc775c70d0de967263d34766ed
chenge <chengethem@gmail.com>
2016-10-30 00:00:55 +0800

    modify logo

---
+++ model/test.js
@@ -0,0 +1,12 @@
+
+
+
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%8D%8E%E8%87%B401.jpg"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%8D%8E%E8%87%B45%20.jpg"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%8D%8E%E8%87%B48.jpg"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%8D%8E%E8%87%B49.jpg"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%8D%8E%E8%87%B46.JPG"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%8D%8E%E8%87%B47.JPG"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%8D%8E%E8%87%B44.jpg"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%8D%8E%E8%87%B43.jpg"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%8D%8E%E8%87%B43.jpg"},
