1c1adfb57b32d4726bdd0fcea42a0611a6f03044
chenge <chengethem@gmail.com>
2016-10-30 23:57:29 +0800

    modify style

---
+++ model/test.js
@@ -0,0 +1,4 @@
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%BE%B7%E5%9B%BD%E5%AE%B6%E5%85%B7%E5%B1%95%E5%8E%85-2.jpg"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%BE%B7%E5%9B%BD%E5%AE%B6%E5%85%B7%E5%B1%95%E5%8E%85-6.jpg"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%BE%B7%E5%9B%BD%E5%AE%B6%E5%85%B7%E5%B1%95%E5%8E%85-8.jpg"},
+{"url":"http://ofsttrt7n.bkt.clouddn.com/%E5%BE%B7%E5%9B%BD%E5%AE%B6%E5%85%B7%E5%B1%95%E5%8E%85-1.jpg"},
